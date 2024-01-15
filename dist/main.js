f6b80acf4d18d67c298edb1980eae39644ded8a1
XxMGxX IT/AI <mohamad7719603@gmail.com>
2024-01-15 15:32:27 +0200

    Add some features for the add button

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,44 @@
 "use strict";
-let Note = document.getElementById("note");
+// [1] Search for "Textarea Element".
+let mainNote = document.getElementById("note");
 let Notes = document.getElementById("notes");
 let AddButton = document.getElementById("add");
+function createNote() {
+    /* #1 Create elements */
+    // The div that contains the note and the buttons
+    let theNote = document.createElement("div");
+    theNote.classList.add("TheNote");
+    // The div that contains the note
+    let Note = document.createElement("textarea");
+    Note.value = (mainNote.value);
+    Note.classList.add("Childnote");
+    // The div that contains the buttons
+    let buttons = document.createElement("div");
+    buttons.classList.add("buttons");
+    // Update button
+    let update = document.createElement("div");
+    let UpText = document.createTextNode("update");
+    update.appendChild(UpText);
+    update.classList.add("update");
+    // delete button
+    let remove = document.createElement("div");
+    let DeText = document.createTextNode("delete");
+    remove.appendChild(DeText);
+    remove.classList.add("delete");
+    /* #1 Create elements */
+    /* #2 Add all elements for their parents */
+    // Add "note" div to "TheNote" div
+    theNote.append(Note);
+    // Add "update" and "delete" buttons to "buttons" div
+    buttons.append(update);
+    buttons.append(remove);
+    // Add "buttons" div to "TheNote" div
+    theNote.append(buttons);
+    /* #2 Add all elements for their parents */
+    /* #3 Return the main div */
+    Notes.append(theNote);
+    /* #3 Return the main div */
+}
 function classNote(div) {
     div.classList.add("note");
 }
@@ -11,8 +48,8 @@ function Editable(div) {
 function UnEditable(div) {
     div.setAttribute("contenteditable", "false");
 }
-function getClone(div) {
-    let copy = Notes.appendChild(div.cloneNode(true));
-    copy.removeAttribute("contenteditable");
+function getClone() {
+    createNote();
 }
+AddButton.onclick = () => getClone();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,45 @@
-let Note = <HTMLDivElement> document.getElementById("note")
+// [1] Search for "Textarea Element".
+
+let mainNote = <HTMLTextAreaElement> document.getElementById("note")
 let Notes = <HTMLDivElement> document.getElementById("notes")
 let AddButton = <HTMLDivElement> document.getElementById("add")
+
+function createNote(): void{
+    /* #1 Create elements */
+    // The div that contains the note and the buttons
+    let theNote = <HTMLDivElement> document.createElement("div")
+    theNote.classList.add("TheNote")
+    // The div that contains the note
+    let Note = <HTMLTextAreaElement> document.createElement("textarea")
+    Note.value = (mainNote.value)
+    Note.classList.add("Childnote")
+    // The div that contains the buttons
+    let buttons = <HTMLDivElement> document.createElement("div")
+    buttons.classList.add("buttons")
+    // Update button
+    let update = <HTMLDivElement> document.createElement("div")
+    let UpText: Text = document.createTextNode("update")
+    update.appendChild(UpText)
+    update.classList.add("update")
+    // delete button
+    let remove = <HTMLDivElement> document.createElement("div")
+    let DeText: Text = document.createTextNode("delete")
+    remove.appendChild(DeText)
+    remove.classList.add("delete")
+    /* #1 Create elements */
+    /* #2 Add all elements for their parents */
+    // Add "note" div to "TheNote" div
+    theNote.append(Note)
+    // Add "update" and "delete" buttons to "buttons" div
+    buttons.append(update)
+    buttons.append(remove)
+    // Add "buttons" div to "TheNote" div
+    theNote.append(buttons)
+    /* #2 Add all elements for their parents */
+    /* #3 Return the main div */
+    Notes.append(theNote)
+    /* #3 Return the main div */
+}
 
 function classNote(div: HTMLDivElement): void{
     div.classList.add("note")
@@ -11,8 +50,8 @@ function Editable(div: HTMLDivElement): void{
 function UnEditable(div: HTMLDivElement): void{
     div.setAttribute("contenteditable", "false")
 }
-function getClone(div: HTMLDivElement): void{
-    let copy = <HTMLDivElement> Notes.appendChild(div.cloneNode(true))
-    copy.removeAttribute("contenteditable")
+function getClone(): void{
+    createNote()
 }
 
+AddButton.onclick = () => getClone()
